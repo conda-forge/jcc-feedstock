46d89651b652575874402ac1968be0ba43c3ee67
petrushy <petrus.hyvonen@gmail.com>
2018-06-10 17:31:44 +0200

    added double array test

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -1,16 +1,13 @@
 "%PYTHON%" test/myrun_test.py
 if errorlevel 1 exit 1
 
-::cd test/java-example
-::javac test/java-example/test1.java
-::jar cf test1.jar test1.class
-::"%PYTHON%" -m jcc --jar test1.jar --python test1 test1 --build --install
-::if errorlevel 1 exit 1
-
 pushd test\java-example
   call "build_and_wrap.bat"
   if errorlevel 1 exit 1
 popd
 
 "%PYTHON%" test/test_test1.py
+if errorlevel 1 exit 1
+
+"%PYTHON%" test/test_double_array.py
 if errorlevel 1 exit 1
--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -2,9 +2,10 @@
 
 $PYTHON test/myrun_test.py
 
-
 pushd test/java-example
   source build_and_wrap.sh
 popd
 
 $PYTHON test/test_test1.py
+$PYTHON test/test_double_array.py
+
