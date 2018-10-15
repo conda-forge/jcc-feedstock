d62f76ade5f3edc4aa5b735158d93abc205f6fa9
petrushy <petrus.hyvonen@gmail.com>
2018-10-15 22:04:10 +0200

    printing the error message on exceptions

--- recipe/test/myrun_test.py
+++ recipe/test/myrun_test.py
@@ -9,13 +9,14 @@ try:
     print ("JCC test OK")
     print ('java version', tmp.java_version)
 
-
-except ImportError:
+except ImportError as e:
     print ('JCC Import Error')
+    print(e)
     sys.exit(1)
 
-except:
+except Exception as e:
     print ('JCC error. Likely your JCC_JDK is wrong..')
+    print(e)
     sys.exit(1)
 
 sys.exit(False)
