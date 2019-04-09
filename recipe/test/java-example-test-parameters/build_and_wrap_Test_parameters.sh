fc2bd5536d6d01b0608e553a6d28912c435d52ab
Petrus Hyvönen <petrus.hyvonen@gmail.com>
2019-04-09 09:07:25 +0200

    removed --use_full_names

--- recipe/test/java-example-test-parameters/build_and_wrap_Test_parameters.sh
+++ recipe/test/java-example-test-parameters/build_and_wrap_Test_parameters.sh
@@ -3,5 +3,5 @@ javac Test_parameter_order.java Animal.java Cat.java Feline.java Mammal.java Sna
 
 jar cf Test_parameter_order.jar Test_parameter_order.class Animal.class Cat.class Feline.class Mammal.class Snake.class
 
-python -m jcc --jar Test_parameter_order.jar --use_full_names --python test2  Test_parameter_order --build --install
+python -m jcc --jar Test_parameter_order.jar --python test2  Test_parameter_order --build --install
 
