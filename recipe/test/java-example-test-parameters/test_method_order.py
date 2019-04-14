dc5bcb52290ea776b9b9c01a90eb36a0819a7d6e
Petrus Hyvönen <petrus.hyvonen@gmail.com>
2019-04-14 08:12:09 +0200

    Added Printouts

--- recipe/test/java-example-test-parameters/test_method_order.py
+++ recipe/test/java-example-test-parameters/test_method_order.py
@@ -15,12 +15,22 @@ assert type(forest.getNewOne(Snake())) == type(Snake())
 print('Single matching OK')
 
 # Double Parameter order check
+print("type(forest.getNewOne(Mammal(), Mammal()))", type(forest.getNewOne(Mammal(), Mammal())))
 assert type(forest.getNewOne(Mammal(), Mammal())) == type(Mammal())
+
+print ("type(forest.getNewOne(Animal(), Animal()))",type(forest.getNewOne(Animal(), Animal())))
 assert type(forest.getNewOne(Animal(), Animal())) == type(Animal())
+
+print ("type(forest.getNewOne(Snake(), Snake()))", type(forest.getNewOne(Snake(), Snake())))
 assert type(forest.getNewOne(Snake(), Snake())) == type(Snake())
+
+print ("type(forest.getNewOne(Feline(), Feline()))", type(forest.getNewOne(Feline(), Feline())))
 assert type(forest.getNewOne(Feline(), Feline())) == type(Feline())
+
+print ("type(forest.getNewOne(Cat(), Cat()))", type(forest.getNewOne(Cat(), Cat())))
 assert type(forest.getNewOne(Cat(), Cat())) == type(Cat())
 
+print ("type(forest.getNewOne(Animal(), Cat()))", type(forest.getNewOne(Animal(), Cat())))
 assert type(forest.getNewOne(Animal(), Cat())) == type(Cat())
 
 print('Double matching OK')
