728aa6fb5692555622d309bd45a7bc3372d3254d
Petrus Hyvönen <petrus.hyvonen@gmail.com>
2019-04-17 18:50:32 +0200

    New test case and separate folders for the two wrapping examples.

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -6,5 +6,12 @@ pushd test/java-example
   source build_and_wrap.sh
 popd
 
-$PYTHON test/test_test1.py
+$PYTHON test/java-example/test_test1.py
+
 $PYTHON test/test_double_array.py
+
+pushd test/java-example-test-parameters
+  source build_and_wrap_Test_parameters.sh
+popd
+
+$PYTHON test/java-example-test-parameters/test_method_order.py
