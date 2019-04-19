f4ad36d7f5c8287dadd1d3e686786da1e5c338f5
Petrus Hyvönen <petrus.hyvonen@gmail.com>
2019-04-19 18:08:35 +0200

    conda tests for updated test cases

--- recipe/test/java-example-test-parameters/build_and_wrap_Test_parameters.sh
+++ recipe/test/java-example-test-parameters/build_and_wrap_Test_parameters.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 javac Test_parameter_order.java Animal.java Cat.java Feline.java Mammal.java Snake.java
 
-jar cf Test_parameter_order.jar Test_parameter_order.class Animal.class Cat.class Feline.class Mammal.class Snake.class
+jar cf Test_parameter_order.jar org
 
-python -m jcc --jar Test_parameter_order.jar --python test2  Test_parameter_order --build --install
+python -m jcc --use_full_names --jar Test_parameter_order.jar --python test2   --package org.jcc.test --build --install
 
