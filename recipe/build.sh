28825e10019747472f0c51cf9ae37f12f16296f0
Petrus Hyvönen <petrus.hyvonen@gmail.com>
2020-07-27 22:29:08 +0200

    Minor updates to syntax, likely not changing anything..

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-export JCC_JDK=$PREFIX
+export JCC_JDK=${PREFIX}
 
 if [ "$(uname)" == "Darwin" ]
 then
   export JCC_ARGSEP=";"
-  export JCC_INCLUDES="$PREFIX/include;$PREFIX/include/darwin"
-  export JCC_LFLAGS="-v;-L$PREFIX/jre/lib;-ljava;-L$PREFIX/jre/lib/server;-ljvm;-Wl,-rpath;-Wl,$PREFIX/jre/lib;-Wl,-rpath;-Wl,$PREFIX/jre/lib/server;-mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET"
+  export JCC_INCLUDES="${PREFIX}/include;${PREFIX}/include/darwin"
+  export JCC_LFLAGS="-v;-L${PREFIX}/jre/lib;-ljava;-L${PREFIX}/jre/lib/server;-ljvm;-Wl,-rpath;-Wl,${PREFIX}/jre/lib;-Wl,-rpath;-Wl,${PREFIX}/jre/lib/server;-mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET"
 	export JCC_CFLAGS="-fno-strict-aliasing;-Wno-write-strings;-Qunused-arguments;-mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET;-std=c++11;-stdlib=libc++"
   export JCC_DEBUG_CFLAGS="-O0;-g;-DDEBUG"
   export JCC_JAVAC="javac;-source;1.8;-target;1.8"
@@ -19,11 +19,10 @@ then
 else
   # GNU/Linux recipe
   export JCC_ARGSEP=";"
-	export JCC_LFLAGS="-v;-Wl,-v;-L$PREFIX/jre/lib/amd64;-ljava;-L$PREFIX/jre/lib/amd64/server;-ljvm;-lverify;-Wl,-rpath=$PREFIX/jre/lib/amd64:$PREFIX/jre/lib/amd64/server"
-	export JCC_JAVAC=$PREFIX/bin/javac
+	export JCC_LFLAGS="-v;-Wl,-v;-L${PREFIX}/jre/lib/amd64;-ljava;-L${PREFIX}/jre/lib/amd64/server;-ljvm;-lverify;-Wl,-rpath=${PREFIX}/jre/lib/amd64:${PREFIX}/jre/lib/amd64/server"
+	export JCC_JAVAC=${PREFIX}/bin/javac
 	export JCC_CFLAGS="-v;-fno-strict-aliasing;-Wno-write-strings;-D__STDC_FORMAT_MACROS"
   export JCC_DEBUG_CFLAGS="-O0;-g;-DDEBUG"
-  export JCC_JAVAC="javac"
   export JCC_JAVADOC="javadoc"
 
 	printenv
