4a385af1bd0f0e2d78c609cbfbbbae7458b146bd
Petrus Hyvönen <petrus.hyvonen@gmail.com>
2020-09-10 23:18:20 +0200

    Pre-expanded paths for minimizing potential issues

--- recipe/scripts/activate.bat
+++ recipe/scripts/activate.bat
@@ -7,12 +7,12 @@
 set "JCC_JDK=%CONDA_PREFIX%\Library"
 
 @set "_JCC_PATH_CONDA_BACKUP=%PATH%"
-@set "PATH=%JCC_JDK%\jre\bin\server;%JCC_JDK%;%JCC_JDK%\jre\bin;%PATH%"
+@set "PATH=%CONDA_PREFIX%\Library\jre\bin\server;%CONDA_PREFIX%\Library;%CONDA_PREFIX%\Library\jre\bin;%PATH%"
 
-:: adding compile parameters explicitly as relocation for conda does not seem to detect JCC  path under windows
-@set "JCC_INCLUDES=%JCC_JDK%\include;%JCC_JDK%\include\win32"
+:: adding compile parameters explicitly as relocation for conda does not seem to detect JCC  path properly
+@set "JCC_INCLUDES=%CONDA_PREFIX%\Library\include;%CONDA_PREFIX%\Library\include\win32"
 @set "JCC_CFLAGS=/EHsc;/D_CRT_SECURE_NO_WARNINGS"
-@set "JCC_LFLAGS=/DLL;/LIBPATH:%JCC_JDK%\lib;Ws2_32.lib;jvm.lib"
+@set "JCC_LFLAGS=/DLL;/LIBPATH:%CONDA_PREFIX%\Library\lib;Ws2_32.lib;jvm.lib"
 @set "JCC_DEBUG_CFLAGS=/Od;/DDEBUG"
-@set "JCC_JAVAC=%JCC_JDK%\bin\javac.exe"
-@set "JCC_JAVADOC=%JCC_JDK%\bin\javadoc.exe"
+@set "JCC_JAVAC=%CONDA_PREFIX%\Library\bin\javac.exe"
+@set "JCC_JAVADOC=%CONDA_PREFIX%\Library\bin\javadoc.exe"
