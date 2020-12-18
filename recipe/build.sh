77a8f9cfc34d136e4fe4584039a04a75fa04a794
Petrus Hyvönen <petrus.hyvonen@gmail.com>
2020-12-18 22:11:15 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,7 @@ if [ "$(uname)" == "Darwin" ]
 then
     export JCC_ARGSEP=";"
     export JCC_INCLUDES="$PREFIX/include;$PREFIX/include/darwin"
-	export JCC_LFLAGS="-v;-L$PREFIX/jre/lib;-ljava;-L$PREFIX/jre/lib/server;-ljvm;-Wl,-rpath;-Wl,$PREFIX/jre/lib;-Wl,-rpath;-Wl,$PREFIX/jre/lib/server;-mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET"
+	export JCC_LFLAGS="-v;-L$PREFIX/lib;-ljava;-L$PREFIX/lib/server;-ljvm;-Wl,-rpath;-Wl,$PREFIX/lib;-Wl,-rpath;-Wl,$PREFIX/lib/server;-mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET"
 	export JCC_CFLAGS="-fno-strict-aliasing;-Wno-write-strings;-Qunused-arguments;-mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET;-std=c++11;-stdlib=libc++"
     export NO_SHARED=1
     printenv
@@ -16,7 +16,7 @@ then
 else
     # GNU/Linux recipe
     export JCC_ARGSEP=";"
-	export JCC_LFLAGS="-v;-Wl,-v;-L$PREFIX/jre/lib/amd64;-ljava;-L$PREFIX/jre/lib/amd64/server;-ljvm;-lverify;-Wl,-rpath=$PREFIX/jre/lib/amd64:$PREFIX/jre/lib/amd64/server"
+	export JCC_LFLAGS="-v;-Wl,-v;-L$PREFIX/lib;-ljava;-L$PREFIX/lib/server;-ljvm;-lverify;-Wl,-rpath=$PREFIX/lib:$PREFIX/lib/server"
 	export JCC_JAVAC=$PREFIX/bin/javac
 	export JCC_CFLAGS="-v;-fno-strict-aliasing;-Wno-write-strings;-D__STDC_FORMAT_MACROS"
 
