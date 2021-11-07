a047edf66c3f56af5a55783325a97dbf77f210fd
Petrus Hyvönen <petrus.hyvonen@gmail.com>
2021-11-07 09:36:32 +0100

    use pip instead of setup.py build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -11,8 +11,8 @@ set "JCC_DEBUG_CFLAGS=/Od;/DDEBUG"
 set "JCC_JAVAC=%JCC_JDK%\bin\javac.exe"
 set "JCC_JAVADOC=%JCC_JDK%\bin\javadoc.exe"
 
-"%PYTHON%" setup.py install --single-version-externally-managed --record record.txt
-::python -m pip install --no-deps --ignore-installed .
+:: "%PYTHON%" setup.py install --single-version-externally-managed --record record.txt
+python -m pip install --no-deps --ignore-installed .
 
 if errorlevel 1 exit 1
 
