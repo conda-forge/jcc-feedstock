dc7d0d2d4be96b103e4571f5b19be81d6157e139
Petrus Hyvönen <petrus.hyvonen@gmail.com>
2022-02-15 08:15:09 +0100

    Latest patch

--- recipe/test/java-example/build_and_wrap.bat
+++ recipe/test/java-example/build_and_wrap.bat
@@ -1,4 +1,4 @@
 javac test1class.java
 jar cf test1class.jar test1class.class
-python -m jcc --jar test1class.jar --python test1  test1class --build --install
+python -m jcc --jar test1class.jar --python test1 --find-jvm-dll client test1class --build --install
 
