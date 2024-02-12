d5bcc293b7148dde08c30db2e77b89b12cd2e06c
Petrus Hyvönen <petrus.hyvonen@sscspace.com>
2024-02-12 08:58:55 +0100

    Using JCC 3.14-beta

--- recipe/test/java-example/build_and_wrap.bat
+++ recipe/test/java-example/build_and_wrap.bat
@@ -1,4 +1,5 @@
 javac test1class.java
 jar cf test1class.jar test1class.class
-python -m jcc --jar test1class.jar --find-jvm-dll server --python test1  test1class --build --install
-
+python -m jcc --jar test1class.jar --find-jvm-dll server --python test1  test1class --generate
+python -m build -nw
+python -m pip install dist/*.whl --force
