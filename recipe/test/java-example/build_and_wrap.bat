53a58007e291457c4f5564f87c541ba7b359d831
Petrus Hyvönen <petrus.hyvonen@sscspace.com>
2024-03-16 10:10:18 +0100

    testing win build

--- recipe/test/java-example/build_and_wrap.bat
+++ recipe/test/java-example/build_and_wrap.bat
@@ -2,4 +2,6 @@ javac test1class.java
 jar cf test1class.jar test1class.class
 python -m jcc --jar test1class.jar --find-jvm-dll server --python test1  test1class --generate
 python -m build -nw
-python -m pip install dist\*.whl --force
+cd dist
+dir
+python -m pip install *.whl --force
